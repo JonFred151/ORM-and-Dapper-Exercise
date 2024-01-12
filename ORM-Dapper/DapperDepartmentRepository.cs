80026e9eebf894a7a289385376d2ad1e1454516b
Jon Frederick <JonFred151@gmail.com>
2024-01-12 16:14:25 -0600

    updated on second video. Time 27:12

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        //local variables to make querys 
+        private readonly IDbConnection _connection;
+        public DapperDepartmentRepository(IDbConnection connection) 
+        {
+            // Constructor
+            _connection = connection;
+
+        }
+
+
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            var depos = _connection.Query<Department>("SELECT * FROM departments ");
+
+            return depos;
+            // second video 27:12 
+        }
+    }
+}
---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public interface IDepartmentRepository
+    {
+        // method called GetAllDepartments that returns a collection
+        // that confirms to IEnumerable<T>
+        IEnumerable<Department> GetAllDepartments();
+
+    }
+}
